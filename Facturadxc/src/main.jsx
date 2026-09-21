2552f7522747e0fd33ca2f247f3a20406f36585f
Jonathan Tambaco <jatambaco@espe.edu.ec>
2026-09-21 15:33:19 -0500

    Delete Facturadxc directory

--- Facturadxc/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { UsuarioAppJT } from './UsuarioAppJT'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <UsuarioAppJT />
-  </StrictMode>,
-)
